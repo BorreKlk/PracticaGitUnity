f1211a4b750969d5260545d427c03598d9a7f82a
Diego <borregaalvarezdiego.animacion@gmail.com>
2024-11-08 12:21:38 +0100

    modificación script

--- Assets/script/git.cs
+++ Assets/script/git.cs
@@ -5,7 +5,7 @@ public class git : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       //hola
     }
 
     // Update is called once per frame
